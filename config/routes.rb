e7990a16af4e428a9fd9a4fe89c2afb346b62499
Sean Collins <sean@cllns.com>
2015-05-05 11:50:45 -0400

    Remove unnecessary route (since it's included in resources)

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   root "posts#index"
 
-  get "posts/index", to: "posts#index"
-
   get "posts/delete", to: "posts#delete"
-
   get "posts/confirm", to: "posts#confirm"
-
   get "posts/success", to: "posts#success"
-
   resources :posts
 
   get "validation_error", to: "posts#validation_error"
