9eaae43bf438a5339be307f77f6e82b7aa630bcf
Mikena Wood <mikena@woodmail.me>
2015-06-05 22:22:11 -0700

    remove street from map link

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem "devise" # for ActiveAdmin only
 gem "email_validator", "~> 1.6.0"
 
 gem "mail_form", "~> 1.5.1"
- 
+
 group :production do
   gem "rails_12factor"
 end
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,5 +1,5 @@
 module SearchHelper
   def maps_query(post)
-    "#{post.street} #{post.city}, #{post.state} #{post.country}"
+    "#{post.city}, #{post.state} #{post.country}"
   end
 end
