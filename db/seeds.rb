7f5dd4efb222c73875f86815afffbd3e06eb3a09
Sean Collins <sean@cllns.com>
2015-06-30 18:14:07 -0400

    Use faker for Expired and Unconfirmed posts too

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,44 +23,49 @@ number_of_fake_posts.times do |i|
     email: email,
     email_confirmation: email,
     expiration: Time.current.utc + 2.weeks,
-    validation: "abc", # SecureRandom.hex,
+    validation: SecureRandom.hex,
     show: true
   )
 
   puts "Created #{(i + 1).ordinalize} fake Post (out of #{number_of_fake_posts})."
 end
 
-puts "Creating expired and unconfirmed post"
+puts "Creating expired and unconfirmed posts"
+
+email = Faker::Internet.email
 
 Post.create!(
   title: "Expired Post",
   post_type: :needed,
-  street: "3350 Adeline St.",
-  city: "Berkeley",
-  state: "CA",
+  street: Faker::Address.street_address,
+  city: Faker::Address.city,
+  state: Faker::Address.state_abbr,
   country: "US",
-  neighborhood: "South Berkeley",
-  description: "text4",
-  email: "foo4@bar.com",
-  email_confirmation: "foo4@bar.com",
-  expiration: Time.current.utc + 1.weeks,
-  validation: "7c745b58bd4e49d81f117ab738f451f4",
+  neighborhood: [Faker::Address.city, ""].sample,
+  description: Faker::Lorem.sentence,
+  email: email,
+  email_confirmation: email,
+  expiration: Time.current.utc - 1.week,
+  validation: SecureRandom.hex,
   show: true
 )
+
+email = Faker::Internet.email
 
 Post.create!(
   title: "Unconfirmed Post",
   post_type: :available,
-  street: "1001 Mission St",
-  city: "San Francisco",
-  state: "CA",
+  street: Faker::Address.street_address,
+  city: Faker::Address.city,
+  state: Faker::Address.state_abbr,
   country: "US",
-  neighborhood: "mission",
-  description: "text1",
-  email: "foo1@bar.com",
-  email_confirmation: "foo1@bar.com",
+  neighborhood: [Faker::Address.city, ""].sample,
+  description: Faker::Lorem.sentence,
+  email: email,
+  email_confirmation: email,
   expiration: Time.current.utc + 2.weeks,
-  validation: "9937c7619c7f376e622a61704ad834c5"
+  validation: SecureRandom.hex,
+  show: false
 )
 
 puts "Done! :)"
