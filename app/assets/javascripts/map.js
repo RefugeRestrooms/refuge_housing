6406d5b61f11a4d3df336fff412772ded17d28bb
Sean Collins <sean@cllns.com>
2015-07-01 21:52:47 -0400

    Only load map JS on posts#show

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,19 +1,21 @@
 $(function() {
-  var latitude = $('#map').data('latitude');
-  var longitude = $('#map').data('longitude');
+  if ($('.posts.show').length) {
+    var latitude = $('#map').data('latitude');
+    var longitude = $('#map').data('longitude');
 
-  var map = L.map('map').setView([latitude, longitude], 13);
-  var RADIUS = 1000;
+    var map = L.map('map').setView([latitude, longitude], 13);
+    var RADIUS = 1000;
 
-  L.circle(L.latLng(latitude, longitude), RADIUS, {
-      opacity: 1,
-      weight: 1,
-      fillOpacity: 0.4
-  }).addTo(map);
+    L.circle(L.latLng(latitude, longitude), RADIUS, {
+        opacity: 1,
+        weight: 1,
+        fillOpacity: 0.4
+    }).addTo(map);
 
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      maxZoom: 18,
-      id: 'seancllns.mjefk2j8',
-      accessToken: 'pk.eyJ1Ijoic2VhbmNsbG5zIiwiYSI6IjgxYmVlZWU5ZWVjYjg2ZTQ2MGFkNmQ2Nzg2YWMyODAyIn0.EwA3kBB4wRZ7LpuCI51_9A'
-  }).addTo(map);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 18,
+        id: 'seancllns.mjefk2j8',
+        accessToken: 'pk.eyJ1Ijoic2VhbmNsbG5zIiwiYSI6IjgxYmVlZWU5ZWVjYjg2ZTQ2MGFkNmQ2Nzg2YWMyODAyIn0.EwA3kBB4wRZ7LpuCI51_9A'
+    }).addTo(map);
+  }
 });
