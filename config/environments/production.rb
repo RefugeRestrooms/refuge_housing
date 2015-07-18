af8a53d55339ea662e5f5749ede98dd6715a2981
Mikena Wood <mikena@woodmail.me>
2015-07-18 13:13:18 -0400

    add domain to prod mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mailer domain
+  config.action_mailer.default_url_options = { host: "http://refugehousing.herokuapp.com/" }
 end
 
 ActionMailer::Base.smtp_settings = {
