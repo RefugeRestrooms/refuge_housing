5d19603499c66885bf3892ee13f6d337316079c9
Mikena Wood <mikena@woodmail.me>
2015-07-30 20:51:43 -0700

    fix linting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,11 +82,11 @@ Rails.application.configure do
 end
 
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
+  address: "smtp.sendgrid.net",
+  port: "587",
+  authentication: :plain,
+  user_name: ENV["SENDGRID_USERNAME"],
+  password: ENV["SENDGRID_PASSWORD"],
+  domain: "heroku.com",
+  enable_starttls_auto: true
 }
